4cc72055752e9c40ca9ddadfc837322365a84235
phamtuandatt <109933954+phamtuandatt@users.noreply.github.com>
2022-10-17 16:49:59 +0700

    first commit - Authetication JWT

---
+++ JWT_Demo/OA_Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using OA_Data;
+
+namespace OA_Service
+{
+    public interface IUserService
+    {
+        // Provide method get User - Insert - Update
+        IEnumerable<Users> GetUsers();
+        Users GetUser(long id);
+        Users CheckLogin(string username, string password);
+        void InsertUser(Users user);
+        void UpdateUser(Users user);
+        void DeleteUser(long id);
+    }
+}
---
+++ JWT_Demo/OA_Web/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OA_Repo;
+using OA_Service;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc();
+
+builder.Services.AddControllers();
+
+builder.Services.AddRazorPages();
+
+// Add EntityModel
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("OA_Repo")
+));
+
+// AddScope
+builder.Services.AddScoped(typeof(IRepo<>), typeof(Repo<>));
+
+// Add Transient
+builder.Services.AddTransient<IUserService, UserService>();
+
+// Add JWT 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issure"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(enpoints =>
+{
+    enpoints.MapControllers();
+    enpoints.MapRazorPages();
+});
+
+app.Run();
